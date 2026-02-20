ea712a6fd0c747f9c2733254ec4b746d5c10d57d
noripi10 <55979374+noripi10@users.noreply.github.com>
2026-02-21 01:51:22 +0900

    create shape script form image conversion

---
+++ src/scripts/shape.ts
@@ -0,0 +1,26 @@
+import { parseArgs } from 'node:util';
+
+import chalk from 'chalk';
+import sharp from 'sharp';
+
+const png2Jpeg = async () => {
+  const { values } = parseArgs({
+    args: process.argv.slice(2),
+    options: {
+      input: { type: 'string', short: 'i' },
+      out: { type: 'string', short: 'o' },
+    },
+  });
+
+  await sharp(values.input).toFile(values.out);
+};
+
+png2Jpeg()
+  .then(() => {
+    console.log(chalk.green('Sharp export image success.'));
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(chalk.red(error));
+    process.exit(1);
+  });
